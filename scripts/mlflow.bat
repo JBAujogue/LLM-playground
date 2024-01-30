6d72fb81b0a221fb291f380e947e52642ac58e3d
Jean-baptiste Aujogue <jb.aujogue@gmail.com>
2024-01-30 22:44:14 +0100

    add mlflow server

---
+++ scripts/mlflow.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+call C:/Users/jb/miniconda3/Scripts/activate.bat
+cd %~dp0../experiments/
+call conda activate llm-playground
+mlflow server --host 127.0.0.1 --port 8080
+call conda deactivate
+pause
