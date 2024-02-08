0adc7bb03a53ac48b829ff00e450c0046dfecaee
Jean-baptiste Aujogue <32547412+JBAujogue@users.noreply.github.com>
2024-02-08 17:51:42 +0100

    Update tensorboard.bat

--- scripts/tensorboard.bat
+++ scripts/tensorboard.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 call C:/Users/jb/miniconda3/Scripts/activate.bat
 cd %~dp0..
-call conda activate transformers_nlp
-tensorboard --logdir=logs
+call conda activate llm-playground
+tensorboard --logdir=mlruns
 call conda deactivate
 pause
