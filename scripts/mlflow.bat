4f1611943ed7c970ef048f12da25d9c2b0cda8f9
Jean-baptiste Aujogue <jb.aujogue@gmail.com>
2024-03-09 20:16:43 +0100

    edit mlflow script

--- scripts/mlflow.bat
+++ scripts/mlflow.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 call C:/Users/jb/miniconda3/Scripts/activate.bat
-cd %~dp0../experiments/logs-mlflow/
+cd %~dp0../mlruns
 call conda activate llm-playground
 mlflow server --host 127.0.0.1 --port 8080
 call conda deactivate
